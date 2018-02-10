30d2d3f165b7a167c78dc181fc83216d20f6e889
Alexandr Kuzmin <shurik@alexandr-kuzmins-macbook.local>
2018-02-09 23:28:04 -0500

    Adding proper transformation of coordinates.

--- raytracing.py
+++ raytracing.py
@@ -3,6 +3,27 @@ import numpy
 # Import pylab to paint
 import pylab
 
-scene = numpy.array([[[0,0,1],[1,0,0]],[[0,1,0],[0,1,0]]])
-pylab.imshow(scene)
+nx = 200
+ny = 200
+scene = numpy.zeros((200,200,3))
+
+#scene = numpy.array([[[0,0,1],[1,0,0]],[[0,1,0],[0,1,0]]])
+#print("Scene=", scene)
+
+# Transformation of coordinates to the window
+lx = 1
+ly = 1
+d = 1
+
+def transform(px, py):
+	x = (py - 0.5 * ny) / ny * lx
+	y = (-px + 0.5 * nx) / nx * ly
+	return [x,y,d]
+
+def intersection(center,direction,tmin,tmax):
+	direction 
+
+print(transform(0,0))	
+print(transform(nx,ny))
+pylab.imshow(scene, interpolation='none')
 pylab.show()
