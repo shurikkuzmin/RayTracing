7c99f9ff8c207d38afee828228caf235b1f43196
Alexandr Kuzmin <shurik@alexandr-kuzmins-macbook.local>
2018-02-10 10:33:37 -0500

    More changes.

--- raytracing.py
+++ raytracing.py
@@ -5,10 +5,11 @@ import pylab
 
 nx = 200
 ny = 200
-scene = numpy.zeros((200,200,3))
+scene = numpy.zeros((nx,ny,3))
 
 #scene = numpy.array([[[0,0,1],[1,0,0]],[[0,1,0],[0,1,0]]])
-#print("Scene=", scene)
+#scene[1,1,:]=[200,0,200]
+#print("Scene=", scene[0,:,2])
 
 # Transformation of coordinates to the window
 lx = 1
@@ -23,7 +24,7 @@ def transform(px, py):
 def intersection(center,direction,tmin,tmax):
 	direction 
 
-print(transform(0,0))	
-print(transform(nx,ny))
+#print(transform(0,0))	
+#print(transform(nx,ny))
 pylab.imshow(scene, interpolation='none')
 pylab.show()
