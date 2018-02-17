dc25cbd784d05630d06998567717234017400414
Alexandr Kuzmin <shurik@pc-admin-30.cimf.ca>
2018-02-17 10:32:21 -0500

    More changes.

--- raytracing.py
+++ raytracing.py
@@ -11,7 +11,7 @@ scene = numpy.zeros((nx,ny,3), dtype=numpy.uint8)
 # Sphere specification
 sphere = {}
 sphere["center"] = numpy.array([0,-1,3])
-sphere["radius"] = 1
+sphere["radius"] = 1.25
 sphere["color"] = numpy.array([200,0,0], dtype=numpy.uint8)
 
 #sphere = {}
@@ -31,27 +31,27 @@ def transform(px, py):
     y = (-px + 0.5 * nx) / nx * ly
     return numpy.array([x,y,d])
 
-background_color = numpy.array([255,255,255],dtype = numpy.uint8)
-def ray(center,direction,tmin,tmax):
-    distance = center - sphere["center"]
-    a = direction.dot(direction)
-    b = 2 * numpy.dot(direction, distance)
-    c = distance.dot(distance) - sphere["radius"]**2
-    discriminant = b**2 - 4*a*c
-    if discriminant > 0:
-        t1 = (-b - numpy.sqrt(discriminant))/(2*a)
-        t2 = (-b + numpy.sqrt(discriminant))/(2*a)
-        if t1 > tmin and t1 < tmax:
-            return sphere["color"]
-        elif t2 > tmin and t2 < tmax:
-            return sphere["color"]
-    return background_color
+# background_color = numpy.array([255,255,255],dtype = numpy.uint8)
+# def ray(center,direction,tmin,tmax):
+#     distance = center - sphere["center"]
+#     a = direction.dot(direction)
+#     b = 2 * numpy.dot(direction, distance)
+#     c = distance.dot(distance) - sphere["radius"]**2
+#     discriminant = b**2 - 4*a*c
+#     if discriminant > 0:
+#         t1 = (-b - numpy.sqrt(discriminant))/(2*a)
+#         t2 = (-b + numpy.sqrt(discriminant))/(2*a)
+#         if t1 > tmin and t1 < tmax:
+#             return sphere["color"]
+#         elif t2 > tmin and t2 < tmax:
+#             return sphere["color"]
+#     return background_color
 
-for i in range(nx):
-    for j in range(ny):
-        direction = transform(i,j)
-        scene[i,j,:] = ray(center, direction, 1.0, 10000.0)
-#print(transform(0,0))  
-#print(transform(nx,ny))
+#for i in range(nx):
+#    for j in range(ny):
+#        direction = transform(i,j)
+#        scene[i,j,:] = ray(center, direction, 1.0, 10000.0)
+print(transform(0,0))  
+print(transform(nx,ny))
 pylab.imshow(scene, interpolation='none')
 pylab.show()
